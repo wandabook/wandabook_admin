553b6539d98c9f7378b16a8b6ae02936a853b363
Your Name <styvesdaudet@gmail.com>
2025-02-18 13:52:25 +0100

    login

--- src/views/LoginView.vue
+++ src/views/LoginView.vue
@@ -11,6 +11,12 @@ const email = ref('');
 const isLoading = ref(false);
 const session = userSessionStore();
 const userStore = useUserStore();
+const passwordVisible = ref(false);
+
+const togglePasswordVisibility = () => {
+    passwordVisible.value = !passwordVisible.value;
+};
+
 const loginUser = async () => {
     try {
         isLoading.value = true;
@@ -46,17 +52,48 @@ onMounted(async () => {
                     </div>
 
                     <form class="bg-grey-lightest px-10 py-10" @submit.prevent="loginUser">
-                        <div class="mb-3">
-                            <input class="border w-full p-3" name="email" type="text" :placeholder="$t('email')"
-                                required v-model="email">
+
+
+                        <!-- Email input -->
+                        <div class="relative mb-4">
+                            <input v-model="email" type="text"
+                                class="peer block  w-full h-13 text-sm text-gray-900 bg-transparent rounded-lg border border-gray-400 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-brand-default focus:outline-none focus:ring-0 focus:border-brand-default px-3 py-2 leading-[1.6] transition-all duration-200 ease-linear"
+                                id="userName" placeholder=" " required />
+                            <label for="userName"
+                                class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-brand-default peer-focus:dark:text-brand-default peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 pointer-events-none">
+                                {{ $t('email') }}
+                            </label>
                         </div>
-                        <div class="mb-6">
-                            <input class="border w-full p-3" name="password" type="password"
-                                placeholder="**************" required v-model="password">
+                        <div class="relative mb-4">
+                            <input v-model="password" :type="passwordVisible ? 'text' : 'password'"
+                                class="peer block h-13 w-full text-sm text-gray-900 bg-transparent rounded-lg border border-gray-400 appearance-none dark:text-white dark:border-gray-600 dark:focus:border-brand-default focus:outline-none focus:ring-0 focus:border-brand-default px-3 py-2 leading-[1.6] transition-all duration-200 ease-linear"
+                                id="password" placeholder=" " required />
+                            <label for="password"
+                                class="absolute text-sm text-gray-500 dark:text-gray-400 duration-300 transform -translate-y-4 scale-75 top-2 z-10 origin-[0] bg-white dark:bg-gray-900 px-2 peer-focus:px-2 peer-focus:text-brand-default peer-focus:dark:text-brand-default peer-placeholder-shown:scale-100 peer-placeholder-shown:-translate-y-1/2 peer-placeholder-shown:top-1/2 peer-focus:top-2 peer-focus:scale-75 peer-focus:-translate-y-4 pointer-events-none">
+                                {{ $t('password') }}
+                            </label>
+                            <!-- Eye icon for password visibility toggle -->
+                            <svg xmlns="http://www.w3.org/2000/svg" fill="none" viewBox="0 0 24 24"
+                                @click="togglePasswordVisibility" v-if="passwordVisible" stroke-width="1.5"
+                                stroke="currentColor"
+                                class="h-6 w-6 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer">
+                                <path stroke-linecap="round" stroke-linejoin="round"
+                                    d="M3.98 8.223A10.477 10.477 0 0 0 1.934 12C3.226 16.338 7.244 19.5 12 19.5c.993 0 1.953-.138 2.863-.395M6.228 6.228A10.451 10.451 0 0 1 12 4.5c4.756 0 8.773 3.162 10.065 7.498a10.522 10.522 0 0 1-4.293 5.774M6.228 6.228 3 3m3.228 3.228 3.65 3.65m7.894 7.894L21 21m-3.228-3.228-3.65-3.65m0 0a3 3 0 1 0-4.243-4.243m4.242 4.242L9.88 9.88" />
+                            </svg>
+
+                            <svg @click="togglePasswordVisibility" xmlns="http://www.w3.org/2000/svg" v-else
+                                class="h-6 w-6 absolute right-2 top-1/2 transform -translate-y-1/2 cursor-pointer"
+                                fill="none" viewBox="0 0 24 24" stroke="currentColor">
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M15 12c0 1.657-1.343 3-3 3s-3-1.343-3-3 1.343-3 3-3 3 1.343 3 3z" />
+                                <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2"
+                                    d="M2.458 12C3.732 7.724 7.758 5 12 5s8.268 2.724 9.542 7c-1.274 4.276-5.3 7-9.542 7-4.242 0-8.268-2.724-9.542-7z" />
+                            </svg>
+
                         </div>
                         <div class="flex">
                             <button type="submit"
-                                class="bg-primary hover:bg-primary-dark w-full p-4 text-sm text-white uppercase font-bold tracking-wider">
+                                class="bg-brand-default hover:bg-primary-dark w-full p-4 text-sm text-white uppercase font-bold tracking-wider">
                                 <svg aria-hidden="true" role="status" v-if="isLoading"
                                     class="inline w-4 h-4 me-3 text-white animate-spin" viewBox="0 0 100 101"
                                     fill="none" xmlns="http://www.w3.org/2000/svg">
@@ -75,7 +112,7 @@ onMounted(async () => {
 
                     <div class="border-t px-10 py-6">
                         <div class="flex justify-between">
-                            <a href="#" class="font-bold text-primary hover:text-primary-dark no-underline">{{
+                            <a href="#" class="font-bold text-brand-default hover:text-primary-dark no-underline">{{
                                 $t('forgot_password') }}</a>
                         </div>
                     </div>
